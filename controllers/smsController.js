49be27058e5a32ebedcc1816b8615518ecc92a82
Kamel Mojawaz <kamel.mojawaz@hotmail.com>
2021-08-29 13:52:56 -0700

    Send sms to my phone number when receive a message on my twilio number

--- controllers/smsController.js
+++ controllers/smsController.js
@@ -5,6 +5,7 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse
 const authToken = process.env.TWILIO_AUTH_TOKEN
 const accountSid = process.env.TWILIO_ACCOUNT_SID
 const sender = process.env.TWILIO_PHONE_NUMBER
+const myPhoneNumber = process.env.MY_PHONE_NUMBER
 const smsServiceUrl = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`
 
 const receiveSms = async (req, res) => {
@@ -12,6 +13,9 @@ const receiveSms = async (req, res) => {
     const twiml = new MessagingResponse()
     twiml.message('The Robots are coming! Head for the hills!')
     res.writeHead(200, { 'Content-Type': 'text/xml' })
+    console.log(req.body)
+    const msg = `${req.body.From} : ${req.body.Body}`
+    await postToTwilio(msg, myPhoneNumber)
     res.end(twiml.toString())
   } catch (error) {
     return res.status(500).json({
@@ -24,24 +28,7 @@ const receiveSms = async (req, res) => {
 const sendSms = async (req, res) => {
   try {
     const { message, recipient } = req.body
-    const data = {
-      Body: message,
-      From: sender,
-      To: recipient,
-    }
-    const encodedData = Object.keys(data)
-      .map(key => `${key}=${encodeURIComponent(data[key])}`)
-      .join('&')
-    const options = {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      auth: {
-        username: accountSid,
-        password: authToken,
-      },
-    }
-    await axios.post(smsServiceUrl, encodedData, options)
+    await postToTwilio(message, recipient)
     return res.status(500).json({
       success: true,
       message: message,
@@ -52,6 +39,27 @@ const sendSms = async (req, res) => {
       message: error,
     })
   }
+}
+
+const postToTwilio = async (message, recipient) => {
+  const data = {
+    Body: message,
+    From: sender,
+    To: recipient,
+  }
+  const encodedData = Object.keys(data)
+    .map(key => `${key}=${encodeURIComponent(data[key])}`)
+    .join('&')
+  const options = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    auth: {
+      username: accountSid,
+      password: authToken,
+    },
+  }
+  await axios.post(smsServiceUrl, encodedData, options)
 }
 
 module.exports = {
--- routers/smsRouter.js
+++ routers/smsRouter.js
@@ -2,6 +2,6 @@ const express = require('express')
 const smsRouter = express.Router()
 const { receiveSms, sendSms } = require('../controllers/smsController')
 
-smsRouter.post('/', sendSms)
-smsRouter.get('/', receiveSms)
+smsRouter.post('/send/', sendSms)
+smsRouter.post('/receive/', receiveSms)
 module.exports = smsRouter
