95af93799e19d56b907c754bb374a36c718f3a71
Kamel Mojawaz <kamel.mojawaz@hotmail.com>
2021-08-29 21:00:32 -0700

    added body-parser

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
 const port = process.env.PORT || 5000
 const cors = require('cors')
 const smsRouter = require('./routers/smsRouter')
 
 app.use(cors())
 app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use('/api/v1/sms/', smsRouter)
 
 const start = () => {
